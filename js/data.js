b499b497851da1b2d6602cfcfe10cbca07273519
Mr. India <mr.india.dev@gmail.com>
2019-07-22 18:33:19 +0530

    Created standard/reusable Dash Board framework

--- js/data.js
+++ js/data.js
@@ -7,7 +7,6 @@ function getData(key, callback) {
 
 }
 
-
 function getUrlData(key, url, callback) {
 
 	$.get({
@@ -18,67 +17,6 @@ function getUrlData(key, url, callback) {
 		}
 	});
 }
-
-function processNSEData(key, data) {
-
-	var chart = charts[key];
-	chart.labels =  data[0].symbol;
-	
-	var dataset = { label : chart.dataset1label, data : [data[0].netPrice] }; 
-	for(var i = 1; i < data.length; i++) {
-		var row = data[i];
-		chart.labels = chart.labels + ',' + row.symbol;
-		dataset.data.push(row.netPrice);
-	}
-	
-	plotChart(key, chart, [ dataset ]);
-
-}
-
-function process91MobileData(key, data) {
-
-	var chart = charts[key];
-	chart.labels =  data[0].name;
-	
-	var dataset = { label : chart.dataset1label, data : [data[0].price] }; 
-	for(var i = 1; i < data.length; i++) {
-		var row = data[i];
-		chart.labels = chart.labels + ',' + row.name;
-		dataset.data.push(row.price);
-	}
-	
-	plotChart(key, chart, [ dataset ]);
-
-}
-
-function processPKLData(key, data) {
-
-	var chart = charts[key];
-	data = data[0].standings.groups[0].teams.team;
-	chart.labels =  data[0].team_name;
-	
-	var dataset = { label : chart.dataset1label, data : [data[0].points] }; 
-	for(var i = 1; i < data.length; i++) {
-		var row = data[i];
-		chart.labels = chart.labels + ',' + row.team_name;
-		dataset.data.push(row.points);
-	}
-	
-	plotChart(key, chart, [ dataset ]);
-
-}
-
-
-getData('1U8oVJ0iDZwPmmbE_wlBq4da7iJ0fyumewOwCW9cvxWY', function(result) {
-	sections = result.data;
-	sections = convertRowsToObj(sections);
-
-	getData('1aXy8j8vPMS0e--U9d8XtPyKdPcA-XG8ao9EHKywSMwQ', function(result) {
-		charts = result.data;
-		charts = convertRowsToObj(charts);
-		onDataLoaded();
-	});
-});
 
 function onDataLoaded() {
 	if(ch) {
