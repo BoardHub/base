1c16ceef1d816b51f26e04b627b960aa8bc36dd3
Mr. India <mr.india.dev@gmail.com>
2020-03-26 14:21:36 +0000

    Enabled pre-populated data selector

--- js/data-selector.js
+++ js/data-selector.js
@@ -1,10 +1,15 @@
 
-function changeDataUrl() {
-	var newSpreadSheetUrl = prompt('Enter Spread Sheet URL');
-	if(newSpreadSheetUrl) {
-		spreadSheetUrl = newSpreadSheetUrl;
-		$('#data_url').val(spreadSheetUrl).trigger('change');
-	}
+// function changeDataUrl() {
+// 	var newSpreadSheetUrl = prompt('Enter Spread Sheet URL');
+// 	if(newSpreadSheetUrl) {
+// 		spreadSheetUrl = newSpreadSheetUrl;
+// 		$('#data_url').val(spreadSheetUrl).trigger('change');
+// 	}
+// }
+
+function changeData() {
+    spreadSheetUrl = $('#data_url').val();
+    loadData();
 }
 
 function loadData() {
@@ -18,7 +23,6 @@ getSheetData(spreadSheetUrl, 1, function(result) {
 		onDataLoaded();
 	});
 });
-
 }
 
 // var header = '';
@@ -41,10 +45,16 @@ getSheetData(spreadSheetUrl, 1, function(result) {
 
 var selector = '';
 selector += '<form class="form-header col-lg-12 p-t-15" onsubmit="return false;">';
-selector += '	<input id="data_url" class="au-input au-input--xl" type="text" name="search" readonly style=";" onchange="loadData()">';
-selector += '	<button class="au-btn--submit" onclick="changeDataUrl()"><i class="zmdi zmdi-search"></i></button>';
+// selector += '	<input id="data_url" class="au-input au-input--xl" type="text" name="search" readonly style=";" onchange="loadData()">';
+// selector += '	<button class="au-btn--submit" onclick="changeDataUrl()"><i class="zmdi zmdi-search"></i></button>';
+selector += '	<select id="data_url" class="form-control-lg form-control" onchange="changeData()">';
+selector += '   	<option value="0">Please Select</option>';
+selector += '   	<option value="155i7b3vhsVUSoQlBrQZpAwX6vXX1JXIlPqpACWP0Xt4">Dash Board</option>';
+selector += '   	<option value="1U8oVJ0iDZwPmmbE_wlBq4da7iJ0fyumewOwCW9cvxWY">Score Board</option>';
+selector += '   	<option value="1-HQI_l2aMsY0pR-xXpj6xgCwuJSI-PSR0Ln-zBoG9rM">Sales Board</option>';
+selector += '	</select>';
 selector += '</form>';
 $('.section__content').prepend($(selector));
 
-var spreadSheetUrl = 'https://docs.google.com/spreadsheets/d/155i7b3vhsVUSoQlBrQZpAwX6vXX1JXIlPqpACWP0Xt4/edit#gid=0';
-$('#data_url').val(spreadSheetUrl).trigger('change');
+// var spreadSheetUrl = 'https://docs.google.com/spreadsheets/d/155i7b3vhsVUSoQlBrQZpAwX6vXX1JXIlPqpACWP0Xt4/edit#gid=0';
+// $('#data_url').val(spreadSheetUrl).trigger('change');
