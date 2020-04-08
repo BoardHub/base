ef4a340eb923093b3b1c59231fbfac0a84bdd252
mrindiadev <mr.india.dev@gmail.com>
2020-04-08 09:05:52 +0000

    Added Tenant path detection

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 var tenantSheetUrl = 'https://docs.google.com/spreadsheets/d/1AlyhZ5EGofE-r6U3G0Eoa852ShsI88TPprIMQJ5UwM8/edit#gid=0';
 var config;
-// var paths = window.location.pathname.split('/');
-// var app;
-// if(paths.length > 1) {
-//     app = paths[paths.length-1];
-// }
 
+var paths = window.location.pathname.split('/');
+var tenant;
+if(paths.length > 1) {
+    tenant = paths[1];
+}
 
 function loadApp() {
 
     getSheetData(tenantSheetUrl, 1, function(result) {
         var configs = convertRowsToObj(result.data);
-        if(app) {
-            config = configs[app];
+        if(tenant) {
+            config = configs[tenant];
         }
         if(!config) {
-            config = configs['tute'];
+            config = configs['default'];
         }
         
         initNav();
